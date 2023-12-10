1a1158b46f04586465776da10b2bfa29f16379c4
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-12-10 17:30:27 +0000

    Change padding for cart

--- src/CartComponents/Cart.jsx
+++ src/CartComponents/Cart.jsx
@@ -3,7 +3,7 @@ import CartCard from "./CartCard";
 import { Outlet } from "react-router-dom";
 
 const cartSyles = {
-  container: "flex flex-col py-2 h-full px-4",
+  container: "flex flex-col py-3 h-full px-4",
 };
 
 export default function Cart() {
